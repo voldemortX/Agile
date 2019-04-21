a70cb5ce510bfc1b69c830f55c055650f01b0fd6
voldemortX <xian_studio@hotmail.com>
2019-04-21 19:23:07 +0800

    Modified SQLs

--- dbCreate.sql
+++ dbCreate.sql
@@ -1,3 +1,14 @@
+# If tables already exist, just drop all of them
+DROP TABLE IF EXISTS `threats`;
+DROP TABLE IF EXISTS `vulnerabilities`;
+DROP TABLE IF EXISTS `assets`;
+DROP TABLE IF EXISTS `systems`;
+DROP TABLE IF EXISTS `users`;
+
+# Create new tables
+# Implement cascade DELETE via foreign keys
+# No unit tests for this directly
+# However, these queries are tested on python library level
 CREATE TABLE `users`
 (
 	`username` varchar(20) PRIMARY KEY,
@@ -11,8 +22,8 @@ CREATE TABLE `systems`
     `method` varchar(20) NOT NULL,
     `results` text NOT NULL,
     `description` text NOT NULL,
-    `createtime` timestamp NOT NULL,
-    FOREIGN KEY (username) REFERENCES users(username),
+    `createtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (username) REFERENCES users(username) ON DELETE CASCADE,
     PRIMARY KEY (systemname, username)
 );
 
@@ -22,7 +33,7 @@ CREATE TABLE `assets`
     `systemname` varchar(50) NOT NULL,
     `val` int NOT NULL,
     `description` text NOT NULL,
-    FOREIGN KEY (systemname) REFERENCES systems(systemname),
+    FOREIGN KEY (systemname) REFERENCES systems(systemname) ON DELETE CASCADE,
     PRIMARY KEY (assetname, systemname)
 );
 
@@ -32,7 +43,7 @@ CREATE TABLE `vulnerabilities`
     `systemname` varchar(50) NOT NULL,
     `val` int NOT NULL,
     `description` text NOT NULL,
-    FOREIGN KEY (systemname) REFERENCES systems(systemname),
+    FOREIGN KEY (systemname) REFERENCES systems(systemname) ON DELETE CASCADE,
     PRIMARY KEY (vulname, systemname)
 );
 
@@ -42,6 +53,6 @@ CREATE TABLE `threats`
     `systemname` varchar(50) NOT NULL,
     `val` int NOT NULL,
     `description` text NOT NULL,
-    FOREIGN KEY (systemname) REFERENCES systems(systemname),
+    FOREIGN KEY (systemname) REFERENCES systems(systemname) ON DELETE CASCADE,
     PRIMARY KEY (threatname, systemname)
 );
