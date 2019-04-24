bae76481ada6d3a5fb9d9af3457eb8c5f8001388
yinwanting628 <1540536081@qq.com>
2019-04-24 22:28:55 +0800

    Minor bugs fixed & '/auth/register' passed ut

--- backend/controllers/authControllers.py
+++ backend/controllers/authControllers.py
@@ -42,4 +42,23 @@ def auth_register_controller():
 @blueprint_auth.route('/login', methods=['POST'])
 @error_guard('/auth/login')
 def auth_login_controller():
-    pass
+    # Get post parameters
+    try:
+        temp_data = request.json
+        username = temp_data['username']
+        password = temp_data['password']
+    except:  # Parameter error
+        current_app.logger.error("Error in parsing requests:", exc_info=True)
+        return jsonify({'status': 1, 'error': HTTP_BADREQ_TEXT}), HTTP_BADREQ
+
+    user=User.query.filter(User.username == username).first()
+    pwd=md5(password)
+    # Check whether this username is existed
+    if user:
+        #Check the password
+        if user.username==username and user.password==pwd:
+            return jsonify({'status': 0}), HTTP_OK
+        else:
+            return jsonify({'status': 1, 'error': '用户名或密码输入错误'}), HTTP_OK
+    else:
+        return jsonify({'status': 1, 'error': '用户名不存在！'}), HTTP_OK
