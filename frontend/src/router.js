2e63b0f46dae722fb8a74317de2b8cbcb8658172
voldemortX <xian_studio@hotmail.com>
2019-04-24 12:14:11 +0800

    Login writtern by element

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from './views/Login.vue'
+import MainPage from './views/MainPage.vue'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'login',
       component: Login
+    },
+    {
+      path: '/main',
+      name: 'mainPage',
+      component: MainPage
     }
   ]
 })
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,30 +1,37 @@
 <template>
     <div>
-        <ul>
-            <li>
-                <el-input v-model="username" placeholder="Enter your name..."></el-input>
-            </li>
-        </ul>
-        <ul>
-            <li>
-                <el-input v-model="password" type="password" placeholder="Enter your password..."></el-input>
-            </li>
-        </ul>
-        <ul>
-            <li>
-                <el-button @click=loginClick>登录</el-button>
-            </li>
-
-        </ul>
-        <ul>
-            <li>
-                <el-button @click=registerClick>注册</el-button>
-            </li>
-        </ul>
-
+        <el-row>
+            <el-col :span="10"><div class="grid-content"></div></el-col>
+            <el-col :span="4">
+                <div class="bg-purple-dark">
+                    <el-row>
+                        <div>
+                            <el-input size="medium" v-model="username" placeholder="Enter your name..."></el-input>
+                        </div>
+                    </el-row>
+                    <el-row>
+                        <div>
+                            <el-input size="medium" v-model="password" type="password" placeholder="Enter your password..."></el-input>
+                        </div>
+                    </el-row>
+                    <el-row>
+                        <el-col :span="12">
+                            <div>
+                                <el-button @click=loginClick>登录</el-button>
+                            </div>
+                        </el-col>
+                        <el-col :span="12">
+                            <div>
+                                <el-button @click=registerClick>注册</el-button>
+                            </div>
+                        </el-col>
+                    </el-row>
+                </div>
+            </el-col>
+            <el-col :span="10"><div class="grid-content"></div></el-col>
+        </el-row>
     </div>
 </template>
-
 
 
 <script>
@@ -33,7 +40,7 @@
         data(){
             return{
                 username: "",
-                password: "",
+                password: ""
 
             }
         }
@@ -41,5 +48,31 @@
 </script>
 
 <style scoped>
+    .el-row {
+        margin-bottom: 10px;
+    }
+    .el-col {
+        border-radius: 4px;
+    }
+    .el-input {
+        margin: 20px;
+        width: 240px;
+    }
+    .el-button {
+        margin: 20px;
+    }
+    .bg-purple-dark {
+        background: #99a9bf;
+    }
+    .bg-purple {
+        background: #d3dce6;
+    }
+    .bg-purple-light {
+        background: #e5e9f2;
+    }
+    .grid-content {
+        border-radius: 4px;
+        min-height: 36px;
+    }
 
 </style>
